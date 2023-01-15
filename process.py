05a437620554247bea77b43e9c05498c09a87f36
Armin Rostami <armin.rostami@tum.de>
2023-01-15 16:37:59 +0100

    Add random search for hyperparams

--- process.py
+++ process.py
@@ -3,6 +3,9 @@ import re
 from hazm import Normalizer, Lemmatizer, InformalNormalizer
 import numpy as np
 import time
+from random import choice, randint
+from multiprocessing import Pool, cpu_count
+import os.path as path
 
 import gensim
 from gsdmm import MovieGroupProcess
@@ -65,10 +68,10 @@ stop_list = [
     "شنبه",
     "یکشنبه",
     "ماه",
-    # "سه",
-    # "انقلاب",
+    "انقلاب",
     # "جمهوری",
     # "اسلام",
+    # "سه",
     #
     # "محمد",
     # "کیان",
@@ -86,6 +89,7 @@ stop_list = [
 
 def remove_hashtags(text):
     text = re.sub("#مهسا_امینی", "", text)
+    # text = re.sub("\n\s*#.*", "", text)
     text = re.sub("\n#\S+", "", text)
     return text
 
@@ -198,7 +202,7 @@ def get_topics_lists(model, top_clusters, n_words):
         topic = []
 
         # iterate over top n words in topic
-        for k, v in sorted_dict:
+        for k, _ in sorted_dict:
             # append words to topic list
             topic.append(k)
 
@@ -208,6 +212,18 @@ def get_topics_lists(model, top_clusters, n_words):
     return topics
 
 
+def get_topic_top_words(cluster_word_distribution, top_cluster, values):
+    res = ""
+    for cluster in top_cluster:
+        sort_dicts = sorted(
+            cluster_word_distribution[cluster].items(),
+            key=lambda k: k[1],
+            reverse=True,
+        )[:values]
+        res += f"\nCluster {cluster} : {sort_dicts}"
+    return res
+
+
 def create_dictionary(docs, remove_n):
 
     dictionary = gensim.corpora.Dictionary(docs)
@@ -215,80 +231,17 @@ def create_dictionary(docs, remove_n):
     dictionary.filter_extremes(no_below=5, no_above=0.5, keep_n=100000)
     dictionary.filter_n_most_frequent(remove_n)
 
-    dictionary.save_as_text("./dict.txt")
+    # dictionary.save_as_text("./dict.txt")
 
     return dictionary
 
 
-def create_gsdmm_model(df, hp):
-    docs = df["tokens"]
-
-    dictionary = create_dictionary(docs, hp["remove_n"])
-
-    vocab_length = len(dictionary)
-
-    gsdmm = MovieGroupProcess(
-        K=hp["num_topics"], alpha=hp["alpha"], beta=hp["beta"], n_iters=hp["iters"]
-    )
-
-    y = gsdmm.fit(docs, vocab_length)
-
-    doc_count = np.array(gsdmm.cluster_doc_count)
-    print("Number of documents per topic :", doc_count)
-
-    # Topics sorted by the number of document they are allocated to
-    top_index = doc_count.argsort()[-15:][::-1]
-    print("Most important clusters (by number of docs inside):", top_index)
-
-    # define function to get top words per topic
-    def top_words(cluster_word_distribution, top_cluster, values):
-        for cluster in top_cluster:
-            sort_dicts = sorted(
-                cluster_word_distribution[cluster].items(),
-                key=lambda k: k[1],
-                reverse=True,
-            )[:values]
-            print("\nCluster %s : %s" % (cluster, sort_dicts))
-
-    # get top words in topics
-    top_words(gsdmm.cluster_word_distribution, top_index, 20)
-
-    topics = get_topics_lists(gsdmm, top_index, 20)
-
-    # evaluate model using Topic Coherence score
-    cm_gsdmm = gensim.models.CoherenceModel(
-        topics=topics,
-        dictionary=dictionary,
-        # corpus=bow_corpus,
-        texts=docs,
-        coherence="c_v",
-    )
-
-    # get coherence value
-    coherence_gsdmm = cm_gsdmm.get_coherence()
-
-    print("\nCoherence: ", coherence_gsdmm)
-
-    return y
-
-
-def main():
-    np.random.seed(1)
-    hparams = {
-        "num_topics": 10,
-        "iters": 10,
-        "alpha": 0.1,
-        "beta": 0.3,
-        "num_tweets": 300000,
-        "remove_n": 50,
-    }
-
-    cols = ["tweet_id", "text", "likes"]
-    df = pd.read_csv("./tweets.csv", usecols=cols)
-    has_likes = df["likes"] > 0
-    df = df[has_likes].head(hparams["num_tweets"])
-
+def preprocess(df):
     start = time.time()
+
+    has_likes = df["likes"] > 0
+    df = df[has_likes]
+
     df = clean_text(df)
     vnorm = np.vectorize(Normalizer().normalize)
     df["clean_text"] = df["clean_text"].apply(vnorm)
@@ -306,20 +259,114 @@ def main():
     df["tokens"] = df["tokens"].apply(np.unique)
 
     df["tokens"] = df["tokens"].apply(delete_stop_words)
+
     df["len"] = df["tokens"].apply(len)
     df = df[df["len"] > 5]
+    print("preprocessing time: ", time.time() - start)
 
-    mid = time.time()
+    return df
 
-    print("starting model creation...")
-    y = create_gsdmm_model(df, hparams)
-    df["topic"] = y
-    end = time.time()
-    print(hparams)
-    df[["tweet_id", "tokens", "text", "topic"]].head(50000).to_csv("./clean.csv")
 
-    print("preprocessing time: ", mid - start)
-    print("modelling time: ", end - mid)
+def create_gsdmm_model(args):
+    start = time.time()
+    df, hp = args
+    np.random.seed(hp["random_state"])
+
+    docs = df["tokens"]
+
+    dictionary = create_dictionary(docs, hp["remove_n"])
+
+    vocab_length = len(dictionary)
+
+    gsdmm = MovieGroupProcess(
+        K=hp["num_topics"], alpha=hp["alpha"], beta=hp["beta"], n_iters=hp["iters"]
+    )
+
+    topic_model = gsdmm.fit(docs, vocab_length)
+    print("modelling time: ", time.time() - start)
+
+    doc_count = np.array(gsdmm.cluster_doc_count)
+    print("Number of documents per topic :", doc_count)
+
+    # Topics sorted by the number of document they are allocated to
+    top_index = doc_count.argsort()[-15:][::-1]
+
+    # print("Most important clusters (by number of docs inside):", top_index)
+    # print(get_topic_top_words(gsdmm.cluster_word_distribution, top_index, 20))
+
+    topics = get_topics_lists(gsdmm, top_index, 20)
+
+    return topic_model, topics, dictionary, hp
+
+
+def get_coherence(topics, dictionary, docs):
+    coherence = -1
+    try:
+        coherence = gensim.models.CoherenceModel(
+            topics=topics,
+            dictionary=dictionary,
+            texts=docs,
+            coherence="c_v",
+        ).get_coherence()
+    except:
+        print("\ncould not calculate coherence.")
+
+    return coherence
+
+
+def random_search(df, num_runs):
+    topic_count = range(4, 30, 2)
+    iter_count = range(5, 30, 5)
+    hprange = [0.01, 0.03, 0.1, 0.2, 0.3, 0.4, 0.5]
+    remove_range = range(0, 80, 10)
+
+    tasks = []
+    for _ in range(num_runs):
+        hparams = {
+            "random_state": randint(0, 100000),
+            "num_topics": choice(topic_count),
+            "iters": choice(iter_count),
+            "alpha": choice(hprange),
+            "beta": choice(hprange),
+            "remove_n": choice(remove_range),
+        }
+        tasks.append((df, hparams))
+
+    NUM_WORKERS = cpu_count()
+    with Pool(NUM_WORKERS) as pool:
+        results = pool.map(create_gsdmm_model, tasks)
+
+    results_df = pd.DataFrame()
+    if path.exists("./hparams.csv"):
+        results_df = pd.read_csv("./hparams.csv", index_col=0)
+
+    max_score = 0
+    best_params = None
+
+    for res in results:
+        topic_model, topics, dictionary, hp = res
+        cs = get_coherence(topics, dictionary, df["tokens"])
+        hp["coherence"] = cs
+        results_df = pd.concat([results_df, pd.DataFrame([hp])], ignore_index=True)
+
+        if cs > max_score:
+            max_score = cs
+            best_params = hp
+
+    results_df.to_csv("./hparams.csv")
+
+    return best_params
+
+
+def main():
+
+    cols = ["tweet_id", "text", "likes"]
+    df = pd.read_csv("./tweets.csv", usecols=cols)
+
+    df = preprocess(df)
+    best_params = random_search(df, 20)
+
+    print("best run params: ", best_params)
 
 
 if __name__ == "__main__":
